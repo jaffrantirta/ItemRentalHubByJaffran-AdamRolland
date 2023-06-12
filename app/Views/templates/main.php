d0f28c4132c0a4ed107e7023ef58f229abfa49fc
jaffrantirta <jaffrantirta@icloud.com>
2023-06-12 16:00:57 +0800

    navbar

--- app/Views/templates/main.php
+++ app/Views/templates/main.php
@@ -10,6 +10,24 @@
 </head>
 
 <body>
+<nav class="bg-gray-800">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex items-center justify-between h-16">
+      <div class="flex items-center">
+        <a href="#" class="text-white text-xl font-bold">Logo</a>
+      </div>
+      <div class="hidden md:block">
+        <div class="ml-10 flex items-baseline space-x-4">
+          <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Beranda</a>
+          <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Keranjang</a>
+          <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Pesanan</a>
+          <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Akun</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
     <?= $this->renderSection('content') ?>
 </body>
 
